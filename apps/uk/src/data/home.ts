e0deaf8d7dc264954a9573bec96fc8b024515241
Sahil Jain <sahiljainsj004@gmail.com>
2026-07-21 18:28:18 +0530

    fix(uk): update customer reviews grid to use about-us-screenshot.png

--- apps/uk/src/data/home.ts
+++ apps/uk/src/data/home.ts
@@ -123,5 +123,5 @@ export const homeCustomerReviewsGrid = {
   copy: "Tons of glowing reviews from customers!",
   ctaLabel: "About Us",
   ctaHref: "/pages/about-us",
-  image: productAsset("01-amazing-hairstyles.webp")
+  image: "/images/home/about-us-screenshot.png"
 };
